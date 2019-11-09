2eafcff1a358a5687bef8268255face0f5ce1f38
iwai32 <iwakou32@gmail.com>
2019-11-09 15:04:31 +0900

    SPA構築のためVue Routerの導入,設定を記述

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,10 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+window.Vue = require('vue')
+import router from './router'
+import App from './App.vue'
+import store from './store/store.js'
 
 /**
  * The following block of code may be used to automatically register your
@@ -18,12 +21,6 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('top-container', require('./components/organisms/TopContainer.vue').default);
-Vue.component('title-logo', require('./components/atoms/TitleLogo.vue').default);
-Vue.component('top-nav', require('./components/molecules/TopNav.vue').default);
-Vue.component('top-nav-link', require('./components/atoms/TopNavLink.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -32,4 +29,8 @@ Vue.component('top-nav-link', require('./components/atoms/TopNavLink.vue').defau
 
 const app = new Vue({
     el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
 });
