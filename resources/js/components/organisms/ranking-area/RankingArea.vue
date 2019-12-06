088fbc5e186b63ef2ead2db5cd8e67a91c798aee
iwai32 <iwakou32@gmail.com>
2019-12-07 01:14:07 +0900

    ランキング画面の仮作成

---
+++ resources/js/components/organisms/ranking-area/RankingArea.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="battle-area">
+    <section-title>RANKING</section-title>
+  </section>
+</template>
+
+<script>
+import SectionTitle from '../../atoms/common/SectionTitle.vue'
+export default {
+  components: {
+    SectionTitle
+  }
+}
+</script>
